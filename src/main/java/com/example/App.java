10ac60501b47929f1e5e16a524996537fd4980a6
Cesar1022 <Cesarramos265@gmail.com>
2025-09-08 22:41:38 -0600

    Added basic project.

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println("Hello World");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/Month.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+}
